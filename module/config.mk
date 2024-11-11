aaa43891fa22f0b8af841c6b023c20363fc12abf
InstallToday <vitalij.malke@gmail.com>
2024-11-11 01:06:29 +0100

    forgot slash

--- module/config.mk
+++ module/config.mk
@@ -87,7 +87,7 @@ CSRCS = \
 	../src/teletype.c					\
 	../src/turtle.c					\
 	../src/chaos.c					\
-	../src/ops/op.c	
+	../src/ops/op.c					\
 	../src/ops/gol.c						\
 	../src/ops/ansible.c					\
 	../src/ops/controlflow.c				\
